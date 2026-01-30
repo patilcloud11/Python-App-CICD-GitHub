36ed6f91dd5985bc486e704f363c12c7dd2424aa
Vishesh Patil <patil.cloud11@gmail.com>
2026-01-30 12:09:01 +0530

    Fix Flask app initialization and main check

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-
 from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "✅ Flask app deployed via GitHub Actions SSH"
 
-if name == "main":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
